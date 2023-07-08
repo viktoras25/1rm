5f61944edf8d512436de55d819de2d56bfcb18ed
Viktoras Bezaras <viktoras.bezaras@gmail.com>
2023-07-08 13:15:37 +0200

    Overflow auto

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ function calculateRepMax() {
 
     let table = '';
 
-    table += '<div class="py-4 overflow-scroll"><table class="table">'
+    table += '<div class="py-4" style="overflow-x: auto"><table class="table">'
 
     table += '<tr>'
     table += '<th>Reps</th>'
